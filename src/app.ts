ab6128ce7d77766f241cd47e1936b252730f78bc
Renato Oliveira <renato.loliveira89@gmail.com>
2024-02-05 10:38:35 -0300

    challenge #2 - 02/02 - Criando rota para testar cadastro de Ave

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import { Ave } from './model/Ave';
 import { Reptil } from './model/Reptil';
 import { Mamifero } from './model/Mamifero';
-import { Animal } from './model/Animal';
 
 const server = express();
 const port = 3000;
@@ -17,6 +16,12 @@ server.get('/', (req, res) => {
     let mamifero: Mamifero = new Mamifero('Doberman', 'Cachorro', 102, 'Femea');
     res.json([ave, reptil, mamifero]);
 })
+
+server.post('/ave', (req, res) => {
+    const { nome, idade, genero, envergadura } = req.body;
+    let ave: Ave = new Ave(nome, idade, genero, envergadura);
+    res.json(["A nova ave do zoológico é: ", ave]);
+});
 
 server.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
